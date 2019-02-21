a38f7fdd82472a4f1dd22174808a5bc81c6b3f51
damlois <lois0168@gmail.com>
2019-02-21 10:25:20 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var app = angular.module('TopupWebApp', [
+    'ngRoute'
+  ]);
+  
+//configure the routes  
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      .when("/", {templateUrl: "partials/singlerecharge.html", controller: "PageCtrl"})
+      .when("/history", {templateUrl: "partials/history.html", controller: "PageCtrl"})
+      .when("/singlerecharge", {templateUrl: "partials/singlerecharge.html", controller: "PageCtrl"})
+      .when("/bulkrecharge", {templateUrl: "partials/bulkrecharge.html", controller: "PageCtrl"})
+      .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+  }]);
+  
